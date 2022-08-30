e322087b1f18527723dc5ad8dad58cdbf223e6b4
UssCringe <110967038+UssCringe@users.noreply.github.com>
2022-08-30 21:16:37 +0200

    Add files via upload

---
+++ 8.sidebar/index.js
@@ -0,0 +1,6 @@
+const bars = document.querySelector(".fa-bars")
+const sidebar = document.querySelector(".sidebar")
+function showHideNav(){
+    console.log("i got clicked")
+    sidebar.classList.toggle("show-sidebar")
+}
